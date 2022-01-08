9c96a86e2358b7142247ba002bc6840138c6cdd7
E. Choroba <choroba@matfyz.cz>
2022-01-08 23:16:29 +0100

    Improve and test ToDot

--- lib/String/Eertree/ToDot.pm
+++ lib/String/Eertree/ToDot.pm
@@ -8,21 +8,22 @@ extends 'String::Eertree';
 
 sub to_dot {
     my ($self) = @_;
-    say 'digraph { rankdir = BT;';
+    my @lines = ('digraph { rankdir = BT;');
     for my $i (0 .. $self->Last) {
         my $node = $self->node($i);
-        say $i, qq([shape=record, label="$i|),
-            $node->string($self) || $i - 1, '"]';
-        say $i, '->', $node->link, '[color=blue]';
+        push @lines, qq($i [shape=record, label="$i|)
+            . ($node->string($self) || $i - 1) . '"]';
+        push @lines, $i . '->' . $node->link . '[color=blue]';
     }
     for my $i (0 .. $self->Last) {
         my $node = $self->node($i);
-        for my $ch (keys %{ $node->edge }) {
-            say $i, '->', $node->edge->{$ch},
-                "[label=$ch, constraint=false]";
+        for my $ch (sort keys %{ $node->edge }) {
+            push @lines, $i . '->' . $node->edge->{$ch}
+                         . "[label=$ch, constraint=false]";
         }
     }
-    say '}';
+    push @lines, '}';
+    return @lines
 }
 
 __PACKAGE__
